f5b9e8aa71ea95d997d3c5761b81d9e686756840
rugieri <rugieri.ronaldo@gmail.com>
2022-02-13 11:33:01 -0300

    added quiz style to flashcard

--- src/components/Quiz/FlashCard.jsx
+++ src/components/Quiz/FlashCard.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import "./Quiz.css";
 
 const FlashCard = ({ question, answer, incrementIndex }) => {
   const [showAnswer, setShowAnswer] = useState(false);
